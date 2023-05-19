2bfbee8d405f371395e7ea36fafb63a189ac23c8
Flávio Marques <flaviomarques2@hotmail.com>
2023-05-19 18:42:58 -0300

    barra de pesquisa funcionando

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ botaoLua.addEventListener('click', () => {
     alert('clicou')
 })
 
+function pesquisaLivro(){
+    let input = document.querySelector('.search').value
+    console.log(input)
+    input=input.toLowerCase(); // Para não haver diferenciação da forma escrita
+    let x = document.getElementsByClassName('livros-estante')
+    console.log(x)
+
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+
+}
+
+pesquisaLivro()
